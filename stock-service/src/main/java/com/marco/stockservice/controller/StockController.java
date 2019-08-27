af14afb5a6187e61e2ec300663742d0edce7f0c9
Marco San Juan <marco.sanjuan@gmail.com>
2019-08-27 19:21:35 +0200

    Enable Hystrix as circuit breaker, to test it, ask for product 0 stock.

--- stock-service/src/main/java/com/marco/stockservice/controller/StockController.java
+++ stock-service/src/main/java/com/marco/stockservice/controller/StockController.java
@@ -1,7 +1,9 @@
 package com.marco.stockservice.controller;
 
+import com.marco.stockservice.model.Product;
 import com.marco.stockservice.model.Stock;
 import com.marco.stockservice.service.StockService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,8 +22,17 @@ public class StockController {
         return stockService.findAllStock();
     }
 
+    @HystrixCommand(fallbackMethod = "alternativeFindStockByProductId")
     @GetMapping("/stock/product/{id}")
-    public Stock findAllStock(@PathVariable Long id){
+    public Stock findStockByProductId(@PathVariable Long id){
         return stockService.findStockByProductId(id);
     }
+
+    public Stock alternativeFindStockByProductId(Long id){
+        Product dummyProduct = new Product();
+        dummyProduct.setName("Dummy Product");
+        dummyProduct.setId(0L);
+        return new Stock(dummyProduct, 0);
+    }
+
 }
