62c3f76457aaf26c6ec549735178d50cccac97bc
Marco San Juan <marco.sanjuan@gmail.com>
2019-09-02 18:27:02 +0200

    Connect stock service with config server, and test it.

--- stock-service/src/main/java/com/marco/stockservice/controller/StockController.java
+++ stock-service/src/main/java/com/marco/stockservice/controller/StockController.java
@@ -5,18 +5,23 @@ import com.marco.stockservice.model.Stock;
 import com.marco.stockservice.service.StockService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class StockController {
 
     @Autowired
     private StockService stockService;
+
+    @Value("${example.config.text}")
+    private String remoteConfigExample;
 
     @GetMapping("/stock/all")
     public List<Stock> findAllStock(){
@@ -37,6 +42,14 @@ public class StockController {
                 .withCreatedAt(new Date())
                 .build();
         return new Stock(dummyProduct, 0);
+    }
+
+    @GetMapping("/stock/config")
+    public ResponseEntity<?> printConfiguration(@Value("${server.port}") String port){
+        final Map<String, String> configMap = new HashMap<>();
+        configMap.put("example", remoteConfigExample);
+        configMap.put("port", port);
+        return new ResponseEntity<Map<String,String>>(configMap, HttpStatus.OK);
     }
 
 }
