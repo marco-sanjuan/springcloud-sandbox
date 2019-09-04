d6a58e0998ce24b9689a4eee64b65f8150a7ff94
Marco San Juan <marco.sanjuan@gmail.com>
2019-09-04 12:06:37 +0200

    How to change configuration without restarting service, using actuator/refresh and @RefreshScope

--- stock-service/src/main/java/com/marco/stockservice/controller/StockController.java
+++ stock-service/src/main/java/com/marco/stockservice/controller/StockController.java
@@ -6,6 +6,7 @@ import com.marco.stockservice.service.StockService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
 
+@RefreshScope
 @RestController
 public class StockController {
 
