8581c84dbb3daf9a9d94c545be4c32f67c484943
Marco San Juan <marco.sanjuan@gmail.com>
2019-09-10 19:16:24 +0200

    Searching with generated rest api, and changing endpoint uris

--- users-service/src/main/java/com/marco/usersservice/persistence/UsersDAO.java
+++ users-service/src/main/java/com/marco/usersservice/persistence/UsersDAO.java
@@ -3,10 +3,14 @@ package com.marco.usersservice.persistence;
 import com.marco.usersservice.model.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "users")
 public interface UsersDAO extends PagingAndSortingRepository<User, Long> {
 
     User findByUserName(String userName);
+
+    @RestResource(path="withEmail")
+    User findByEmail(String email);
 
 }
