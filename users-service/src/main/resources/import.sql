2e1c1784f015ab0bd7377c0a0444c5e88e4e7e98
Marco San Juan <marco.sanjuan@gmail.com>
2019-09-10 15:46:43 +0200

    Default records for users table

---
+++ users-service/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into users (user_name, password, first_name, last_name, enabled, email) VALUES ('admin','12345', 'Admin', 'sysadmin', 1, 'admin@test.com');
+insert into users (user_name, password, first_name, last_name, enabled, email) VALUES ('markzuck','12345', 'Mark', 'Zuck', 1, 'mzuck@test.com');
+insert into users (user_name, password, first_name, last_name, enabled, email) VALUES ('lisasmith','12345', 'Lisa', 'Smith', 1, 'lisa@test.com');
+insert into users (user_name, password, first_name, last_name, enabled, email) VALUES ('georgedoe','12345', 'George', 'Doe', 1, 'george@test.com');
+insert into users (user_name, password, first_name, last_name, enabled, email) VALUES ('johngo','12345', 'John', 'Goodman', 1, 'john@test.com');
