6093ae9323a914d9482e502c2d6e4616140bd33d
Marco San Juan <marco.sanjuan@gmail.com>
2019-09-10 15:49:45 +0200

    Auto export repository to Rest API HATEOAS (spring-data-rest module)   Route from zuul.

--- users-service/src/main/java/com/marco/usersservice/persistence/UsersDAO.java
+++ users-service/src/main/java/com/marco/usersservice/persistence/UsersDAO.java
@@ -2,7 +2,9 @@ package com.marco.usersservice.persistence;
 
 import com.marco.usersservice.model.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "users")
 public interface UsersDAO extends PagingAndSortingRepository<User, Long> {
 
     User findByUserName(String userName);
